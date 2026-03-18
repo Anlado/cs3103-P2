24f4e86a6c6e6ebffebc8f451c1da4e11cb4c214
Anlado <mithil.honkanadavar@gmail.com>
2026-03-18 14:50:39 -0400

    changes, mostly UI

--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -1,16 +1,60 @@
 using System.Collections.Generic;
+using TMPro;
+using Unity.Jobs;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour
 {
     List<InventoryItem> Inventory;
+    public Button[] invButtons;
+    public Button[] QSButtons;
+    private InventoryItem[] QSPointers;
+
+    private bool quickSlotSelection;
+    private int tempButtonID;
 
     Stats playerStats;
 
+    public TextMeshProUGUI invText;
+
     void Start()
     {
+        QSPointers = new InventoryItem[4];
+        System.Array.Fill(QSPointers, null);
         Inventory = new List<InventoryItem>();
         playerStats = new Stats(100f);
+        updateInvUI();
+        quickSlotSelection = false;
+        tempButtonID = -1;
+        clearText();
+    }
+
+    private void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            QSButtonPress(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            QSButtonPress(1);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            QSButtonPress(2);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            QSButtonPress(3);
+
+        }
+        */
+
     }
 
     private bool addItem(InventoryItem item)
@@ -40,6 +84,8 @@ public class InventoryScript : MonoBehaviour
             {
                 Destroy(collision.gameObject);
                 printInventoryContents();
+                updateInvUI();
+
             }
         }
         else if (collision.gameObject.tag == "MedHealth")
@@ -48,6 +94,7 @@ public class InventoryScript : MonoBehaviour
             {
                 Destroy(collision.gameObject);
                 printInventoryContents();
+                updateInvUI();
 
             }
         }
@@ -57,8 +104,21 @@ public class InventoryScript : MonoBehaviour
             {
                 Destroy(collision.gameObject);
                 printInventoryContents();
+                updateInvUI();
+
             }
         }
+        else
+        {
+            invText.text = "Inventory Full";
+            CancelInvoke(nameof(clearText)); 
+            Invoke(nameof(clearText), 2f);
+        }
+    }
+
+    private void clearText()
+    {
+        invText.text = "";
     }
 
     private void useItem(string itemName)
@@ -75,9 +135,8 @@ public class InventoryScript : MonoBehaviour
                 playerStats.changeHealth(30);
                 break;
         }
+
     }
-
-
 
     private void printInventoryContents()
     {
@@ -86,4 +145,130 @@ public class InventoryScript : MonoBehaviour
             print(item.name);
         }
     }
+
+    private void updateInvUI()
+    {
+        //Update Inv UI
+        for (int i = 0; i < 12; i++)
+        {
+            TextMeshProUGUI tmp = invButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            if (i < 6)
+            {
+                if (i >= Inventory.Count)
+                {
+                    tmp.text = "Empty";
+                }
+                else
+                {
+                    tmp.text = Inventory[i].name + ": " + Inventory[i].count;
+                }
+            }
+            else
+            {
+                if (i >= Inventory.Count)
+                {
+                    tmp.text = "Empty";
+                    invButtons[i].gameObject.SetActive(false);
+                }
+                else
+                {
+                    tmp.text = Inventory[i].name + ": " + Inventory[i].count;
+                    invButtons[i].gameObject.SetActive(true);
+                }
+            }
+        }
+
+        //Update QS UI
+        for (int i = 0; i < 4; i++)
+        {
+            TextMeshProUGUI QStmp = QSButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+
+            if (QSPointers[i] == null)
+            {
+                QStmp.text = "QS: " + (i+1);
+            }
+            else
+            {
+                string itemName = QSPointers[i].name;
+                int itemCount = QSPointers[i].count;
+                QStmp.text = itemName + ": " + itemCount;
+            }
+        }
+    }
+
+    public void startQuickSelect()
+    {
+        quickSlotSelection = !quickSlotSelection;
+
+        if (quickSlotSelection)
+        {
+            invText.text = "Press Inventory Slot then press QuickSelect slot";
+            tempButtonID = -1;
+        }
+        else
+        {
+            clearText();
+        }
+    }
+
+    public void invButtonPress(int buttonID)
+    {
+        if (!quickSlotSelection)
+        {
+            if (buttonID >= Inventory.Count)
+            {
+                return;
+            }
+
+            useItem(Inventory[buttonID].name);
+
+            InventoryItem item = Inventory[buttonID];
+            if (item.count <= 1)
+            {
+                Inventory.Remove(item);
+            }
+            else
+            {
+                item.count--;
+            }
+            updateInvUI();
+            Debug.Log(item.name);
+        }
+        else
+        {
+            tempButtonID = buttonID;
+        }
+    }
+
+    public void QSButtonPress(int QSButtonID)
+    {
+        if (!quickSlotSelection)
+        {
+            if (QSPointers[QSButtonID] == null)
+            {
+                return;
+            }
+
+            int buttonID = Inventory.IndexOf(QSPointers[QSButtonID]);
+            invButtonPress(buttonID);
+
+            if (!Inventory.Contains(QSPointers[QSButtonID]))
+            {
+                QSPointers[QSButtonID] = null;
+            }
+        }
+        else
+        {
+            if (tempButtonID < Inventory.Count)
+            {
+                QSPointers[QSButtonID] = Inventory[tempButtonID];
+            }
+
+            quickSlotSelection = !quickSlotSelection;
+            clearText();
+        }
+
+        updateInvUI();
+    }
+
 }
